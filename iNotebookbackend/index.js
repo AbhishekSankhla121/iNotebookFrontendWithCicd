186741a98450048d72b616ec69fef22c113a675f
abhishek sankhla <abhisheksankhla121@gmail.com>
2025-08-18 13:59:08 +0530

    fix

--- iNotebookbackend/index.js
+++ iNotebookbackend/index.js
@@ -17,5 +17,5 @@ app.use("/notes", require('./Routes/notes'));
 app.use("/image", express.static('./temp'))
 
 app.listen(port, () => {
-    console.log(`listen portsssssss : http://localhosts:${port}`)
+    console.log(`listen portsssssssss : http://localhosts:${port}`)
 }) 
