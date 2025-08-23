1e1e937900de8af790f1c403e5696b01d33de277
abhishek sankhla <abhisheksankhla121@gmail.com>
2025-08-23 18:53:56 +0530

    check backend again

--- iNotebookbackend/index.js
+++ iNotebookbackend/index.js
@@ -17,5 +17,5 @@ app.use("/notes", require('./Routes/notes'));
 app.use("/image", express.static('./temp'))
 
 app.listen(port, () => {
-    console.log(`listen porsssssa : http://localhosts:${port}`)
+    console.log(`listen porsssssass : http://localhosts:${port}`)
 }) 
