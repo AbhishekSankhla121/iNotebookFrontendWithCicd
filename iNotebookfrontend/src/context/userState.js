43490ae920824d14cec28170dd569b814dcc4047
abhishek sankhla <abhisheksankhla121@gmail.com>
2025-08-23 20:08:58 +0530

    fix docker frontend

--- iNotebookfrontend/src/context/userState.js
+++ iNotebookfrontend/src/context/userState.js
@@ -57,7 +57,7 @@ const Userstate = (props) => {
             }
 
             if (json.message) {
-                console.log("hello")
+                console.log("hellod")
                 Alert({ alertMessage: `Error:${json.message.errors[0].msg}`, alertColor: "#EC7063" });
             }
             return json;
